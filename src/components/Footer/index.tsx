3c399e05da49543a2a158d1ce4201bb9a05d1ad7
Enrique Novoa <luisenriquenovoa@gmail.com>
2024-12-11 12:00:00 -0600

    nits

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 
   return (
     <div className="flex flex-col justify-between bg-secondary-grey px-2 sm:px-4 md:px-10 lg:px-20 xl:px-40 py-6 items-center text-sm">
-      <div className="w-[calc(100%-3rem)] md:w-3/4 flex mb-6 mt-3 justify-evenly">
+      <div className="w-full flex mb-6 mt-3 justify-evenly">
         <div className="flex flex-col">
           <Button className="" href="/privacy-policy" type="link">
             Privacy Policy
@@ -24,14 +24,15 @@ export default function Footer() {
           <Button href="/about" type="link">
             Services
           </Button>
+          <Button href="/faqs" type="link">
+            FAQs
+          </Button>
           {/* <p>Careers</p> TODO! */}
         </div>
         <div className="flex flex-col text-b">
           <div>
-            <Button href="/about" type="link">
-              Contact us
-            </Button>{" "}
-            <br /> Phone{" "}
+            <span className="font-semibold">Contact us:</span>
+            <br /> Phone:{" "}
             <a
               className="text-blue-500 underline hover:text-blue:400"
               href="tel:+15126414433"
